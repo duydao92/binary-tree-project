e25ab6895b4cf494f5e75ce8c71196e966bf657e
duy <ddao0892@gmail.com>
2021-02-02 15:41:44 -0700

    final

--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -4,26 +4,36 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
-
-    //obj = {val: preorder[0]
-        //   left: null
-        //   right: null}
-    //
+    if (!preorder.length && !inorder.length) return null;
 
     let splitpoint = inorder.indexOf(preorder[0]);
     let leftInorder = inorder.slice(0, splitpoint);
-    let rightInorder = inorder.slice(splitpoint+1, inorder.length);
+    let rightInorder = inorder.slice(splitpoint + 1);
+    preorder.shift();
+    let leftPreorder = preorder.slice(0, leftInorder.length);
+    let rightPreorder = preorder.slice(leftInorder.length);
 
-    return { "val": inorder(splitpoint), "left": buildTree(preorder, leftInorder), "right": buildTree(preorder, rightInorder) }
+    return { "val": inorder[splitpoint], "left": buildTree(leftPreorder, leftInorder), "right": buildTree(rightPreorder, rightInorder) }
 }
 
-preorder = [3,9,20,15,7]
-inorder = [9,3,15,20,7]
-
-preorder2 = [20, 10, 12, 34, 28, 49, 36]
-inorder2 = [10, 12, 20, 28, 34, 36, 49]
-
-preorder3 = [12, 15, 5, 24, 8, 17, 10];
-inorder3 = [5, 24, 15, 12, 17, 8, 10]
-//compare preorder2[i] to inorder2[i+1] = same
-    // preorder2[i+1] < preorder2[i]
+function buildTree(preorder, inorder) {
+    function helper(start,end){
+        if (start > end){
+            return null
+        }
+        let rootVal = preorder.shift()
+        let index = inorder_index_map[rootVal]
+        let root = new TreeNode(rootVal)
+        root.left = helper(start, index - 1)
+        root.right = helper(index + 1, end)
+        return root
+    }
+    //the object will store num:idx so we can have O(1)
+    // lookup when we are getting the index of the root
+    // in the inorder array
+    inorder_index_map = {}
+    for (let i = 0; i < inorder.length; i++){
+        inorder_index_map[inorder[i]] = i
+    }
+    return helper(0,inorder.length - 1)
+}
