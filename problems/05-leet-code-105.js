d315f1650cb5082cfd19a7c203e71a944329ad4a
duy <ddao0892@gmail.com>
2021-02-02 14:15:42 -0700

    tree node from preorder and inorder

--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -4,5 +4,18 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
+    //preorder = queue
+    //inorder = result
+
+    //obj = {val: preorder[0]
+        //   left: null
+        //   right: null}
+    //
 
 }
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+
+preorder2 = [20, 10, 12, 34, 28, 49, 36]
+inorder2 = [10, 12, 20, 28, 34, 36, 49]
